867e0a855a573ea935d0313b43b73646d029ba0e
Stephen <Stephen@oregonburls.com>
2017-03-16 14:55:46 -0700

    inital commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('client'));
+
+app.listen(80, () => {
+	console.log('listening on port 80');
+});
